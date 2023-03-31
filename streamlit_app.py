10d6f121f9e11d3a2c8569534fdc9be78ae6547a
wrotki8778 <44114906+wrotki8778@users.noreply.github.com>
2023-03-31 08:43:21 +0200

    Add captions etc.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.caption("Omega 3 & Blueberry Oatmeal")
+streamlit.caption("Kale, Spinach & Rocket Smoothie")
+streamlit.caption("Hard-Boiled Free-Range Egg")
