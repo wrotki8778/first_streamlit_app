b26e0ccb7e759a639ffcafe8573f618c4b8e37a1
wrotki8778 <44114906+wrotki8778@users.noreply.github.com>
2023-03-31 08:46:00 +0200

    Add funny emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.caption("Omega 3 & Blueberry Oatmeal")
-streamlit.caption("Kale, Spinach & Rocket Smoothie")
-streamlit.caption("Hard-Boiled Free-Range Egg")
+streamlit.header("Breakfast Menu")  
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
