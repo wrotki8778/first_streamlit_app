22885869bb170c808b37a444a46efc800f57fc2b
wrotki8778 <44114906+wrotki8778@users.noreply.github.com>
2023-04-04 08:46:44 +0200

    Improving code part 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import pandas
 import requests
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
 streamlit.title("My Parents New Healthy Diner")
 streamlit.header("Breakfast Menu")  
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -31,9 +36,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(get_fruityvice_data(fruit_choice))
 except URLError as e:
     streamlit.error()
 
