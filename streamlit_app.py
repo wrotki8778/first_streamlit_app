0daf0c8081fabd511a0a9a2a56702f64488c80b3
wrotki8778 <44114906+wrotki8778@users.noreply.github.com>
2023-04-04 09:51:38 +0200

    Improving code part 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,16 +40,20 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+
 streamlit.text("The fruit load list contains:")
+def get_fruit_load_list():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+
 streamlit.text(my_data_rows)
 
+streamlit.stop()
 fruit_choice_insert = streamlit.text_input('What fruit would you like to add?','Durian')
 my_cur.execute("Insert into fruit_load_list (fruit_name) values ('" + fruit_choice_insert + "');")
 
